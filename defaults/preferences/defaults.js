ee9be9253752517358f25e731447d3a2f5df82fb
Danny Farrell <dan@cyrusbio.com>
2023-03-10 17:06:06 -0800

    Change default PSM

--- defaults/preferences/defaults.js
+++ defaults/preferences/defaults.js
@@ -6,5 +6,5 @@ pref("extensions.zotero.zoteroocr.outputHocr", true);
 pref("extensions.zotero.zoteroocr.outputPNG", true);
 pref("extensions.zotero.zoteroocr.maximumPagesAsHtml", "5");
 pref("extensions.zotero.zoteroocr.outputDPI", "300");
-pref("extensions.zotero.zoteroocr.PSMMode", "1");
+pref("extensions.zotero.zoteroocr.PSMMode", "3");
 pref("extensions.zotero.zoteroocr.outputAsCopyAttachment", true);

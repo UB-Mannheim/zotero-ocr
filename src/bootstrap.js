0554a53a21d992fa6b00452e188a461f349408f1
Stefan Weil <sw@weilnetz.de>
2024-03-20 07:43:05 +0100

    Add more log messages to bootstrap.js
    Signed-off-by: Stefan Weil <sw@weilnetz.de>

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -10,16 +10,21 @@ function install() {
 
 async function startup({ id, version, rootURI }) {
 	log("Starting");
-	log("id=" + id + ", version=" + version + ", rootURI=" + rootURI);
+	// log("id=" + id + ", version=" + version + ", rootURI=" + rootURI);
 
+	log("Starting (register)");
 	Zotero.PreferencePanes.register({
 		pluginID: 'zotero-ocr@uni-mannheim.de',
 		src: rootURI + 'prefs.xhtml'
 	});
 
+	log("Starting (loadSubScript)");
 	Services.scriptloader.loadSubScript(rootURI + 'chrome/content/zoteroocr.js');
+	log("Starting (init)");
 	ZoteroOCR.init({ id, version, rootURI });
+	log("Starting (addToAllWindows)");
 	ZoteroOCR.addToAllWindows();
+	log("Starting finished");
 }
 
 function onMainWindowLoad({ window }) {

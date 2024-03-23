b68c4abcf2e0a9f092466d080f7337012dca9102
Stefan Weil <sw@weilnetz.de>
2024-03-23 12:37:37 +0100

    Simplify build script and don't remove old build results
    Signed-off-by: Stefan Weil <sw@weilnetz.de>

--- build.sh
+++ build.sh
@@ -5,11 +5,5 @@ if [ -z "$version" ]; then
 	read -p "Enter new version number: " version
 fi
 
-if [ -d build ]; then
-	if [ -f build/*.xpi ]; then
-		rm -f  build/*.xpi
-	fi
-else
-	mkdir build
-fi
-cd src && zip -r ../build/zotero-ocr-${version}.xpi * -x "*.DS_Store" && cd ..
+mkdir -p build
+(cd src && zip -r ../build/zotero-ocr-${version}.xpi * -x ".*")

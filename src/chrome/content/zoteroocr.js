d9920d3cd89970e4dea3a2ddcd134c12b033b6a0
Alain Borel <alain.borel@epfl.ch>
2025-02-09 10:10:45 +0100

    use JPEG instead of PNG as intermediate format

--- src/chrome/content/zoteroocr.js
+++ src/chrome/content/zoteroocr.js
@@ -133,6 +133,26 @@ Zotero.OCR = new function() {
 			let ocrbase = Zotero.Prefs.get("zoteroocr.overwritePDF") ? base : base + '.ocr';
 			// TODO filter out PDFs which have already a text layer
 
+			// build the pdftoppm arguments based on hidden preferences:
+            // => will produce a PDF output with reasonable size and image quality
+            // File format: JPEG by default instead of PNG
+            // JPEG quaility 70/100 (pdftoppm default is 75)
+            // JPEG Hufmann tables optimization: yes (pdftoppm default is no)
+            // Use progressive JPEF: yes (pdftoppm default is no)
+            let imageFormat = Zotero.Prefs.get("zoteroocr.imageFormat");
+            let pdftoppmCmdArgs;
+            if (imageFormat == "jpg" || imageFormat == "jpeg") {
+                imageFormat = "jpg";
+                let jpegQuality = Zotero.Prefs.get("zoteroocr.jpegQuality");
+                let jpegProgressive = Zotero.Prefs.get("zoteroocr.jpegProgressive");
+                let jpegOptimization = Zotero.Prefs.get("zoteroocr.jpegOptimization");
+                pdftoppmCmdArgs = ['-jpeg', '-jpegopt', 'quality='+jpegQuality+',progressive='+jpegProgressive+',optimize='+jpegOptimization, '-r', Zotero.Prefs.get("zoteroocr.outputDPI"), pdf, dir + '/page'];
+
+            } else {
+                imageFormat = "png";
+                pdftoppmCmdArgs = ['-png', '-r', Zotero.Prefs.get("zoteroocr.outputDPI"), pdf, dir + '/page'];
+            }
+
 			// extract images from PDF
 			let imageList = OS.Path.join(dir, 'image-list.txt');
 			if (!(yield OS.File.exists(imageList))) {
@@ -141,7 +161,6 @@ Zotero.OCR = new function() {
 					Zotero.debug("Running " + pdfinfo + ' ' + pdfinfoCmdArgs.join(' '));
 					yield Zotero.Utilities.Internal.exec(pdfinfo, pdfinfoCmdArgs);
 
-					let pdftoppmCmdArgs = ['-png', '-r', Zotero.Prefs.get("zoteroocr.outputDPI"), pdf, dir + '/page'];
 					Zotero.debug("Running " + pdftoppm + ' ' + pdftoppmCmdArgs.join(' '));
 					yield Zotero.Utilities.Internal.exec(pdftoppm, pdftoppmCmdArgs);
 				}

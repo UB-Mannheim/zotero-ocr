UB-Mannheim/zotero-ocr

commit d18035f280eb38102f2e7a5aba37add2077755ae

Improve release handling
- Preserve original timestamps of unmodified files.
- Create new tag only if there were changes.

Signed-off-by: Stefan Weil <sw@weilnetz.de>

release.sh
@@ -10,8 +10,16 @@ fi
 ## Update install.rdf and manifest.json
 ##############
 
-perl -pi -e "s/em:version=\"[^\"]*/em:version=\"$version/;" src/install.rdf
-perl -pi -e "s/\"version\": \"[^\"]*\"/\"version\": \"$version\"/" src/manifest.json
+perl -pi.bak -e "s/em:version=\"[^\"]*/em:version=\"$version/;" src/install.rdf
+if cmp -s src/install.rdf.bak src/install.rdf; then
+  # Nothing changed, restore original file and timestamp.
+  mv src/install.rdf.bak src/install.rdf
+fi
+perl -pi.bak -e "s/\"version\": \"[^\"]*\"/\"version\": \"$version\"/" src/manifest.json
+if cmp -s src/manifest.json.bak src/manifest.json; then
+  # Nothing changed, restore original file and timestamp.
+  mv src/manifest.json.bak src/manifest.json
+fi
 
 
 ##############
@@ -23,8 +31,8 @@ perl -pi -e "s/\"version\": \"[^\"]*\"/\"version\": \"$version\"/" src/manifest.
 perl -pi -e "s/\"version\": \"[^\"]*\",/\"version\": \"${version}\",/" updates.json
 perl -pi -e "s/\"update_link\": \"[^\"]*\",/\"update_link\": \"https:\/\/github.com\/UB-Mannheim\/zotero-ocr\/releases\/download\/${version}\/zotero-ocr-${version}.xpi\",/" updates.json
 perl -pi -e "s/\"update_hash\": \"[^\"]*\",/\"update_hash\": \"sha256:$(shasum -a 256 build\/zotero-ocr-${version}.xpi | cut -d' ' -f1)\",/" updates.json
-cp updates.json update.rdf
+cp -p update.rdf updates.json
 
 git add src/install.rdf src/manifest.json update.rdf updates.json
-git commit -m "Release $version" 1>&2
+git commit -m "Release $version" &&
 git tag -a -m "Release $version" "$version"

commit 99840021619c4156633653c6584b814ac0296ed8

Fix bug in release script
Fixes: d18035f280eb ("Improve release handling")
Signed-off-by: Stefan Weil <sw@weilnetz.de>

release.sh
@@ -31,7 +31,7 @@ fi
 perl -pi -e "s/\"version\": \"[^\"]*\",/\"version\": \"${version}\",/" updates.json
 perl -pi -e "s/\"update_link\": \"[^\"]*\",/\"update_link\": \"https:\/\/github.com\/UB-Mannheim\/zotero-ocr\/releases\/download\/${version}\/zotero-ocr-${version}.xpi\",/" updates.json
 perl -pi -e "s/\"update_hash\": \"[^\"]*\",/\"update_hash\": \"sha256:$(shasum -a 256 build\/zotero-ocr-${version}.xpi | cut -d' ' -f1)\",/" updates.json
-cp -p update.rdf updates.json
+cp -p updates.json update.rdf
 
 git add src/install.rdf src/manifest.json update.rdf updates.json
 git commit -m "Release $version" &&
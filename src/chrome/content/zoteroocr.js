0b6ee3814a28ca4f27a4e4dbe2697ab8eeb6997c
aborel <alain.borel@epfl.ch>
2025-11-24 19:19:46 +0100

    Update src/chrome/content/zoteroocr.js
    Co-authored-by: Stefan Weil <sw@weilnetz.de>

--- src/chrome/content/zoteroocr.js
+++ src/chrome/content/zoteroocr.js
@@ -337,7 +337,7 @@ Zotero.OCR = new function() {
                 const pageRegex = /Page (\d+) :/
                 let string
             
-                // Detect errors but ignpore most Leptonica messages
+                // Detect errors but ignore most Leptonica messages
                 // reported by functions such as boxClipToRectangle, pixScanForForeground...
                 // indicating recognition problems on one page or another but no critical tessrract failure.
                 // Based on the function names in Leptoninca's src/allheaders.h
